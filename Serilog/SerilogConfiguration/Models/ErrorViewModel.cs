5c592f16eccd5f7edaf0821bd70309a50b9e2411
shohag66 <professional.cse66@gmail.com>
2022-08-30 03:43:22 +0600

    SerilogonfigurationProjectCreate

---
+++ Serilog/SerilogConfiguration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SerilogConfiguration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
